903c40720e699e07995646d2d57918dfd4b0ee29
xxzxws <412124495@qq.com>
2019-03-08 16:55:58 +0800

    3月8日更新  rbac  stark   删除 编辑按钮权限，下一步进行详细的部署设置，对照视频增加界面权限设置

---
+++ rbac/stark.py
@@ -0,0 +1,17 @@
+from stark.service.v1 import site
+from rbac.models import *
+
+# class UserInfo(models.Model):
+#
+
+site.register(UserInfo)
+# class Role(models.Model):
+#
+site.register(Role)
+# class Permission(models.Model):
+#
+site.register(Permission)
+# class Menu(models.Model):
+
+site.register(Menu)
+
